e5d8c0bea938ed9e50900e2f01b159cbeb8ee305
conortyler98 <conortyler@Debs-MacBook-Pro-2.local>
2016-11-18 23:17:38 +0000

    Readded main menu

--- app/src/main/java/com/capae/cotd/costofthediet/ContinentSelector.java
+++ app/src/main/java/com/capae/cotd/costofthediet/ContinentSelector.java
@@ -9,5 +9,6 @@ public class ContinentSelector extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_continent_selector);
+        getActionBar().setTitle("Select Continent");
     }
 }
