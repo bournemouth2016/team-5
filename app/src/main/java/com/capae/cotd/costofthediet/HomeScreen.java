07d66c0767d6707d41e6726b66e459d0509be76c
conortyler98 <conortyler@Debs-MacBook-Pro-2.local>
2016-11-18 22:30:56 +0000

    Added path to activity

--- app/src/main/java/com/capae/cotd/costofthediet/HomeScreen.java
+++ app/src/main/java/com/capae/cotd/costofthediet/HomeScreen.java
@@ -1,7 +1,9 @@
 package com.capae.cotd.costofthediet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+    }
+
+    public void goContinent(View view){
+        Intent intent = new Intent(this, ContinentSelector.class);
+        startActivity(intent);
     }
 }
