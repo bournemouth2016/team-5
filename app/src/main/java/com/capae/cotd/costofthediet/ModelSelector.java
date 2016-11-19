4cb71354f263830000f4bed68a1b8bad06707e43
conortyler98 <conortyler@Debs-MacBook-Pro-2.local>
2016-11-19 02:26:45 +0000

    Added path to diet for 1st model

--- app/src/main/java/com/capae/cotd/costofthediet/ModelSelector.java
+++ app/src/main/java/com/capae/cotd/costofthediet/ModelSelector.java
@@ -1,7 +1,9 @@
 package com.capae.cotd.costofthediet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ModelSelector extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ModelSelector extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_model_selector_africa);
+    }
+
+    public void goDiet(View view){
+        Intent intent = new Intent(this, DietKenya.class);
+        startActivity(intent);
     }
 }
