1e4cdae37425dbb8e2c4c3a5d93c4c1aec571f38
conortyler98 <conortyler@Debs-MacBook-Pro-2.local>
2016-11-19 02:10:08 +0000

    Added drawables

---
+++ app/src/main/java/com/capae/cotd/costofthediet/ModelSelector.java
@@ -0,0 +1,13 @@
+package com.capae.cotd.costofthediet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ModelSelector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_model_selector_africa);
+    }
+}
