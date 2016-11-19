d786bad425f6338f4486b83b9565e73666b3cc10
conortyler98 <conortyler@Debs-MacBook-Pro-2.local>
2016-11-19 01:40:39 +0000

    Designed basic layout for Food menu

---
+++ app/src/main/java/com/capae/cotd/costofthediet/FoodGroupDir.java
@@ -0,0 +1,13 @@
+package com.capae.cotd.costofthediet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoodGroupDir extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_group_dir);
+    }
+}
