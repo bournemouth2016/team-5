be644740b1527271054ac47a72fc953be9579308
emmmelvin <e_melvin@hotmail.co.uk>
2016-11-19 04:07:11 +0000

    Variables Saving
    basic

--- app/src/main/java/com/capae/cotd/costofthediet/FamilySize.java
+++ app/src/main/java/com/capae/cotd/costofthediet/FamilySize.java
@@ -9,5 +9,21 @@ public class FamilySize extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family_size);
+        String continent = ContinentSelector.continent;
+        String country = AfricaSelector.country;
+        if (continent == "Africa" && country == "Kenya")
+        {
+            String childFoodWeight = "4985";
+            String womanFoodWeight = "6942";
+            String manFoodWeight = "5752";
+        }
+        else if (continent == "" && country == "")
+        {
+            //Nothing
+        }
+        else
+        {
+            //Shouldn't be here
+        }
     }
 }
