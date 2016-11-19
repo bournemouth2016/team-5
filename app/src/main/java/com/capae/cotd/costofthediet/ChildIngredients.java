16bce74501b80ab01bde08d5e4dfbaeb8a66b0c2
emmmelvin <e_melvin@hotmail.co.uk>
2016-11-19 07:16:25 +0000

    Ingredients Update
    Adding table and button function

--- app/src/main/java/com/capae/cotd/costofthediet/ChildIngredients.java
+++ app/src/main/java/com/capae/cotd/costofthediet/ChildIngredients.java
@@ -2,6 +2,11 @@ package com.capae.cotd.costofthediet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ChildIngredients extends AppCompatActivity {
 
@@ -9,5 +14,12 @@ public class ChildIngredients extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child_ingredients);
+    }
+
+    public void ShowTable(View view) {
+
+        View vTbl = (ImageView) findViewById(R.id.imageView3);
+
+        vTbl.setVisibility(View.VISIBLE);
     }
 }
